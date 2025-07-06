5131a7ba83ed884fefebda7c5581f4599ba17c2f
Codibex <Michael_H@live.at>
2025-07-06 19:46:15 +0200

    Remove obsolete endpoint

--- src/MCP.Host/Endpoints.cs
+++ src/MCP.Host/Endpoints.cs
@@ -7,22 +7,6 @@ public static class Endpoints
 {
     public static void MapEndpoints(this WebApplication app)
     {
-        // Map the weather forecast endpoint
-        app.MapGet("/weatherforecast", () =>
-        {
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                )).ToArray();
-            return forecast;
-        }).WithName("GetWeatherForecast");
 
         app.MapPost("/chat", (async (ChatRequest request, Kernel kernel) =>
         {
