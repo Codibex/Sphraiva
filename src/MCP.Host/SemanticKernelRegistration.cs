0841b4c79653089327cc75b29ab68086caa887fa
Codibex <Michael_H@live.at>
2025-07-06 16:34:04 +0200

    Register mcp in host

--- src/MCP.Host/SemanticKernelRegistration.cs
+++ src/MCP.Host/SemanticKernelRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.SemanticKernel;
+using ModelContextProtocol.Client;
 using OllamaSharp;
 
 namespace MCP.Host;
@@ -14,5 +15,28 @@ public static class SemanticKernelRegistration
             .AddOllamaChatCompletion(ollamaClient)
             .AddOllamaTextGeneration(ollamaClient)
             .AddOllamaEmbeddingGenerator(ollamaClient);
+
+        var kernel = kernelBuilder.Build();
+
+        var tools = GetToolsAsync().GetAwaiter().GetResult();
+
+        
+        kernel.Plugins.AddFromFunctions("Sphraiva", tools.Select(t => t.AsKernelFunction()));
+
+        services.AddSingleton(kernel);
+        services.AddSingleton(ollamaClient);
+    }
+
+    private static async Task<IList<McpClientTool>> GetToolsAsync()
+    {
+        await Task.Delay(5000);
+        await using var mcpClient = await McpClientFactory.CreateAsync(new SseClientTransport(new SseClientTransportOptions()
+        {
+            Endpoint = new Uri("http://sphraiva-mcp-server:8080/"),
+            TransportMode = HttpTransportMode.AutoDetect
+        }));
+
+        var tools = await mcpClient.ListToolsAsync().ConfigureAwait(false);
+        return tools;
     }
 }
--- src/MCP.Server/FileSystemTool.cs
+++ src/MCP.Server/FileSystemTool.cs
@@ -3,24 +3,38 @@ using ModelContextProtocol.Server;
 [McpServerToolType]
 public static class FileSystemTool
 {
-    [McpServerTool]
-    public static Task<object?> ReadFile(dynamic parameters)
+    [McpServerTool(Title = "Read file", Destructive = false, Idempotent = true, ReadOnly = true, UseStructuredContent = true)]
+    public static Task<ReadFileResult> ReadFile(ReadFileParameters parameters)
     {
-        string? path = parameters?.path as string;
+        string? path = parameters.Path;
         if (string.IsNullOrWhiteSpace(path))
             throw new ArgumentException("Path is required");
         var content = File.ReadAllText(path);
-        return Task.FromResult<object?>(new { content });
+        return Task.FromResult(ReadFileResult.SuccessResult(content));
     }
 
-    [McpServerTool]
-    public static Task<object?> WriteFile(dynamic parameters)
+    [McpServerTool(Title = "Write file", Destructive = false, Idempotent = false, ReadOnly = false, UseStructuredContent = true)]
+    public static Task<WriteFileResult> WriteFile(WriteFileParameters parameters)
     {
-        string? path = parameters?.path as string;
-        string? content = parameters?.content as string;
+        string? path = parameters.Path;
+        string? content = parameters.Content;
         if (string.IsNullOrWhiteSpace(path))
             throw new ArgumentException("Path is required");
         File.WriteAllText(path, content ?? "");
-        return Task.FromResult<object?>(new { success = true });
+        return Task.FromResult(WriteFileResult.SuccessResult());
     }
+}
+
+public record ReadFileParameters(string Path);
+public record ReadFileResult(string? Content, string? ErrorMessage = null)
+{
+    public static ReadFileResult SuccessResult(string content) => new(content);
+    public static ReadFileResult Failure(string errorMessage) => new(null, errorMessage);
+}
+
+public record WriteFileParameters(string Path, string Content);
+public record WriteFileResult(bool Success, string? ErrorMessage = null)
+{
+    public static WriteFileResult SuccessResult() => new(true);
+    public static WriteFileResult Failure(string errorMessage) => new(false, errorMessage);
 }
