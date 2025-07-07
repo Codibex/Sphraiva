88c4cfbd6a43ad6384880a6f1596cb9491b960d0
Codibex <Michael_H@live.at>
2025-07-07 22:32:27 +0200

    Use MudBlazor

--- src/MCP.WebApp/MCP.WebApp/Program.cs
+++ src/MCP.WebApp/MCP.WebApp/Program.cs
@@ -1,4 +1,5 @@
 using MCP.WebApp.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped<IMcpService, McpService>();
 
