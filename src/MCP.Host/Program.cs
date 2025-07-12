1fcd2d43c71dd5e7d0fb61c0dd549b2cb67ce855
Codibex <Michael_H@live.at>
2025-07-12 23:04:23 +0200

    Add logging

--- src/MCP.Host/Program.cs
+++ src/MCP.Host/Program.cs
@@ -2,6 +2,11 @@ using MCP.Host;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddLogging(services => services
+    .AddConsole()
+    .SetMinimumLevel(LogLevel.Trace)
+);
+
 builder.Services.AddSemanticKernel(builder.Configuration);
 
 // Add services to the container.
