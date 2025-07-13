69595e91e64cdb030cd312b47dd962aa92d2517c
Codibex <Michael_H@live.at>
2025-07-13 14:22:05 +0200

    Add background worker

---
+++ src/MCP.BackgroundWorker.FileSystem/Program.cs
@@ -0,0 +1,7 @@
+using MCP.BackgroundWorker.FileSystem;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
