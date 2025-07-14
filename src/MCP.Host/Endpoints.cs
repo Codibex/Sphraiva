dc3b499133c016fc8807209d9c4ef5c299f5a147
Codibex <Michael_H@live.at>
2025-07-14 17:17:56 +0200

    Improve performance

--- src/MCP.Host/Endpoints.cs
+++ src/MCP.Host/Endpoints.cs
@@ -63,10 +63,14 @@ public static class Endpoints
 #pragma warning restore SKEXP0130
 #pragma warning restore SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-            ICollection<ChatMessageContent> messages = [];
-            messages = await agent
+            var messages = new List<ChatMessageContent>();
+            await agent
                 .InvokeAsync(request.Message, _agentThread, cancellationToken: cancellationToken)
-                .AggregateAsync(messages, (current, response) => [..current, response.Message], cancellationToken: cancellationToken);
+                .AggregateAsync(messages, (current, response) =>
+                {
+                    current.Add(response.Message);
+                    return current;
+                }, cancellationToken: cancellationToken);
             
             var response = string.Join(Environment.NewLine, messages.Select(m => m.Content));
             return Results.Ok(response);
